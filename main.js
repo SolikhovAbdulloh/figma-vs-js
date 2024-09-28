1439f0b857edeccd569f6884dc77c05767fcc876
SolihovAbdulloh <abdullasolikh@mail.com>
2024-09-28 09:26:22 +0500

    start

---
+++ main.js
@@ -0,0 +1,76 @@
+// let obj = {
+//     name:"abdulloh",
+//     age:12,
+// }
+
+// let obj2 = {...obj,fullname:obj.name + 'bek'}
+
+// console.log(obj2);
+
+// bu class declaration
+
+//  class a  {  
+//     constructor(name,age,price){
+//         this.name = name;
+//         this.age = age;
+//         this.price = price;
+
+//     }
+// }
+
+// console.log(new a('as',12,123));
+
+
+// bu class declaration
+
+// bu birikitirilgan ozgaruchidan ochadi malumotlarni undan oladi
+
+//  let b = class a  {  
+//     constructor(name,age,price){
+//         this.name = name;
+//         this.age = age;
+//         this.price = price;
+
+//     }
+// }
+
+// console.log(new b('as',12,123));
+
+
+// bu class anonim expression (nomsiz )
+
+// let a = class  {  
+//     constructor(name,age,price){
+//         this.name = name;
+//         this.age = age;
+//         this.price = price;
+
+//     }
+// }
+
+// console.log(new a('asadulla',12,123));
+
+
+// # bu belgi malumotni undefined qiladi malumot korinmaydi
+
+// class User {
+//     // #yosh;
+//     constructor(ism,yosh){
+//         this.ism = ism;
+//         this.yosh = yosh;
+//     }
+
+//     set setFuc(yosh){
+//          this.yosh = yosh;
+//     }
+
+// }
+ 
+// let person = new User('Abdulloh','89')
+
+// //ARRAY ORQALI MUROJAT QILISH
+// person.setFuc = yosh[0]
+// console.log(person);
+
+
+// console.log(person.getFuc);
